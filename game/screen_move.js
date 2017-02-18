9078dfa1e141fbd55bb8631dc615c107d5a2a635
MX-Steve <2513556525@qq.com>
2017-02-18 17:10:28 +0800

    a new game

---
+++ game/screen_move.js
@@ -0,0 +1,34 @@
+var game={
+	PSIZE:300,
+	OFFSET:56,
+	MAXTOP:0,
+	MAXLEFT:0,
+	pop:null,
+	init:function(){
+		this.MAXTOP=screen.availHeight-this.PSIZE-this.OFFSET;
+		this.MAXLEFT=screen.availWidth-this.PSIZE;
+		var rTop=Math.floor(Math.random()*(this.MAXTOP+1));
+		var rLeft=Math.floor(Math.random()*(this.MAXLEFT+1));
+		var config="top="+rTop+",left="+rLeft+",width="+this.PSIZE+",height="+this.PSIZE;
+		this.pop=open("about:blank","_blank",config);
+		var img=new Image();
+		img.src="E://MX-Steve.github.io/game/xiaoxin.jpg";
+		img.style.width=this.PSIZE-20+"px";
+		img.onclick=function(){alert("约吗？")}
+		this.pop.document.body.appendChild(img);
+		this.pop.onmouseover=function(e){
+			while(true){
+				var rTop=Math.floor(Math.random()*(this.MAXTOP+1));
+				var rLeft=Math.floor(Math.random()*(this.MAXLEFT+1));
+				var x=e.screenX;
+				var y=e.screenY;
+				if(!(x>=rLeft&&x<=rLeft+this.PSIZE
+					&&y>=rTop&&y<=rTop+this.OFFSET+this.PSIZE)){
+				this.pop.moveTo(rLeft,rTop);
+				break;
+				}
+			}
+		}.bind(this);
+	}
+}
+game.init();
